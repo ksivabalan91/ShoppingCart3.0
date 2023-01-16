9aa0b9d1c5b950a2db2f0133cbf27754d45f5996
K.Sivabalan <ksivabalan.91@gmail.com>
2023-01-16 16:18:26 +0800

    code clean up

--- src/cart3/Cart.java
+++ src/cart3/Cart.java
@@ -1,12 +1,6 @@
 package cart3;
-import java.util.LinkedList;
-import java.util.List;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
